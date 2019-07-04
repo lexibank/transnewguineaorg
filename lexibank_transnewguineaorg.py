e7f0f70be21ea93796a1d44a77394ddeff0e98b4
Christoph Rzymski <christoph@functor-argument.net>
2019-07-04 12:38:10 +0200

    Code cleanup, Glottolog 4.0, release 1.0.1.

--- lexibank_transnewguineaorg.py
+++ lexibank_transnewguineaorg.py
@@ -1,18 +1,13 @@
-# coding=utf-8
-from __future__ import unicode_literals, print_function
-import re
-from os import remove
-from datetime import datetime
 import unicodedata
+from datetime import datetime
+from os import remove
 
-import lingpy
+from clldutils.path import Path
+from clldutils.text import strip_brackets
 from pybtex.database import parse_string  # dependency of pycldf, so should be installed.
 from pybtex.utils import OrderedCaseInsensitiveDict
-from clldutils.text import strip_brackets
-from clldutils.path import Path
-from pylexibank.util import get_url, jsondump
-from pylexibank.dataset import Metadata
 from pylexibank.dataset import Dataset as BaseDataset
+from pylexibank.util import get_url, jsondump
 
 LIMIT = 2000  # how many records to fetch at once
 
@@ -28,77 +23,78 @@ class Dataset(BaseDataset):
     id = "transnewguineaorg"
     dir = Path(__file__).parent
 
-    def get_slug_from_uri(self, uri):
+    @staticmethod
+    def get_slug_from_uri(uri):
         return [_ for _ in uri.split("/") if _][-1]
 
     def clean_form(self, row, form):
         form = strip_brackets(form.strip())
         form = unicodedata.normalize("NFC", form)
-        #form = form.replace("*", "")  # normalise protoforms
+        # form = form.replace("*", "")  # normalise protoforms
         return form
 
     def cmd_install(self, **kw):
-        languages = {o['slug']: o for o in self.raw.read_json(Path(self.raw, "languages.json"))}
-        words = {o['slug']: o for o in self.raw.read_json(Path(self.raw, "words.json"))}
-        sources = {o['slug']: o for o in self.raw.read_json(Path(self.raw, "sources.json"))}
-        
+        languages = {o["slug"]: o for o in self.raw.read_json(Path(self.raw, "languages.json"))}
+        words = {o["slug"]: o for o in self.raw.read_json(Path(self.raw, "words.json"))}
+        sources = {o["slug"]: o for o in self.raw.read_json(Path(self.raw, "sources.json"))}
+
         with self.cldf as ds:
             # handle sources
             # want to make sure that the bibtex key matches our source id.
             self.log.info("Loading sources...")
             for s in sorted(sources):
                 # this is ugly, I wish pybtex made this easier!
-                bib = parse_string(sources[s]['bibtex'], 'bibtex')
+                bib = parse_string(sources[s]["bibtex"], "bibtex")
                 old_key = bib.entries.keys()[0]
                 bib.entries[old_key].key = s
                 bib.entries = OrderedCaseInsensitiveDict([(s, bib.entries[old_key])])
                 ds.add_sources(bib)
-            
+
             # handle languages
             self.log.info("Loading languages...")
             for l in sorted(languages):
                 ds.add_language(
                     ID=l,
-                    Name=languages[l]['fullname'],
-                    ISO639P3code=languages[l]['isocode'],
-                    Glottocode=languages[l]['glottocode']
+                    Name=languages[l]["fullname"],
+                    ISO639P3code=languages[l]["isocode"],
+                    Glottocode=languages[l]["glottocode"],
                 )
-            
+
             # handle concepts
             self.log.info("Loading concepts...")
             for c in sorted(words):
                 ds.add_concept(
                     ID=c,
-                    #Local_ID=words[c]['id'],
-                    Name=words[c]['word'],
-                    Concepticon_ID=words[c]['concepticon_id'],
-                    Concepticon_Gloss=words[c]['concepticon_gloss']
+                    # Local_ID=words[c]['id'],
+                    Name=words[c]["word"],
+                    Concepticon_ID=words[c]["concepticon_id"],
+                    Concepticon_Gloss=words[c]["concepticon_gloss"],
                 )
-            
+
             itemfiles = [f for f in self.raw.iterdir() if f.name.startswith("language-")]
             for filename in sorted(itemfiles):
                 self.log.info("Loading data from %s..." % filename)
-                for o in sorted(self.raw.read_json(filename), key=lambda d: d['id']):
-                    for value in self.split_forms(o, o['entry']):
+                for o in sorted(self.raw.read_json(filename), key=lambda d: d["id"]):
+                    for value in self.split_forms(o, o["entry"]):
                         ds.add_lexemes(
-                            #ID=o['id'],
-                            Local_ID=o['id'],
-                            Language_ID=self.get_slug_from_uri(o['language']),
-                            Parameter_ID=self.get_slug_from_uri(o['word']),
+                            # ID=o['id'],
+                            Local_ID=o["id"],
+                            Language_ID=self.get_slug_from_uri(o["language"]),
+                            Parameter_ID=self.get_slug_from_uri(o["word"]),
                             Value=value,
                             Form=self.clean_form(o, value),
-                            Source=self.get_slug_from_uri(o['source']),
-                            Comment=o['annotation']
+                            Source=self.get_slug_from_uri(o["source"]),
+                            Comment=o["annotation"],
                         )
 
     def get_all(self, url):
         while True:
             j = get_url(url, self.log).json()
-            yield j['objects']
-            if not j['meta']['next']:
+            yield j["objects"]
+            if not j["meta"]["next"]:
                 break
-            url = BASE_URL + j['meta']['next']
-    
+            url = BASE_URL + j["meta"]["next"]
+
     def cmd_download(self, **kw):
         if not self.raw.exists():
             self.raw.mkdir()
@@ -108,30 +104,29 @@ class Dataset(BaseDataset):
 
         # sources
         sources = []
-        for j in self.get_all(SOURCES_URL % {'limit': LIMIT}):
+        for j in self.get_all(SOURCES_URL % {"limit": LIMIT}):
             sources.extend(j)
         jsondump(sources, Path(self.raw, "sources.json"), self.log)
 
         # languages
         languages = []
-        for j in self.get_all(LANGUAGES_URL % {'limit': LIMIT}):
+        for j in self.get_all(LANGUAGES_URL % {"limit": LIMIT}):
             languages.extend(j)
         jsondump(languages, Path(self.raw, "languages.json"), self.log)
 
         # words
         words = []
-        for j in self.get_all(WORDS_URL % {'limit': LIMIT}):
+        for j in self.get_all(WORDS_URL % {"limit": LIMIT}):
             words.extend(j)
         jsondump(words, Path(self.raw, "words.json"), self.log)
 
         # items
         for language in languages:
             items = []
-            for j in self.get_all(RECORDS_URL % {'limit': LIMIT, 'language': language['id']}):
+            for j in self.get_all(RECORDS_URL % {"limit": LIMIT, "language": language["id"]}):
                 items.extend(j)
-            jsondump(items, Path(self.raw, "language-%d.json" % language['id']), self.log)
-        
+            jsondump(items, Path(self.raw, "language-%d.json" % language["id"]), self.log)
+
         # version information
-        with open(Path(self.raw, "version.txt"), 'w') as handle:
+        with open(Path(self.raw, "version.txt"), "w") as handle:
             handle.write(str(datetime.now()))
-        
--- test.py
+++ test.py
@@ -1,41 +1,37 @@
-# coding: utf-8
-from __future__ import unicode_literals
-
-
 def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
 
 
-def test_forms(cldf_dataset, cldf_logger):
+def test_forms(cldf_dataset):
     # check one specific form to make sure columns, values are correct.
-    #49995,karoto,father,aɸa
-    f = [f for f in cldf_dataset['FormTable'] if f['Local_ID'] == '49995']
+    # 49995,karoto,father,aɸa
+    f = [f for f in cldf_dataset["FormTable"] if f["Local_ID"] == "49995"]
     assert len(f) == 1
-    assert f[0]['Parameter_ID'] == 'father'
-    assert f[0]['Language_ID'] == 'karoto'
-    assert f[0]['Form'] == 'aɸa'
+    assert f[0]["Parameter_ID"] == "father"
+    assert f[0]["Language_ID"] == "karoto"
+    assert f[0]["Form"] == "aɸa"
 
 
-def test_languages(cldf_dataset, cldf_logger):
-    #ID,Name,Glottocode,Glottolog_Name,ISO639P3code,Macroarea,Family
-    #abaga,Abaga,abag1245,,abg,,
-    f = [f for f in cldf_dataset['LanguageTable'] if f['ID'] == 'abaga']
+def test_languages(cldf_dataset):
+    # ID,Name,Glottocode,Glottolog_Name,ISO639P3code,Macroarea,Family
+    # abaga,Abaga,abag1245,,abg,,
+    f = [f for f in cldf_dataset["LanguageTable"] if f["ID"] == "abaga"]
     assert len(f) == 1
-    assert f[0]['Name'] == 'Abaga'
-    assert f[0]['Glottocode'] == 'abag1245'
-    assert f[0]['ISO639P3code'] == 'abg'
+    assert f[0]["Name"] == "Abaga"
+    assert f[0]["Glottocode"] == "abag1245"
+    assert f[0]["ISO639P3code"] == "abg"
 
 
-def test_parameters(cldf_dataset, cldf_logger):
-    #ID,Name,Concepticon_ID,Concepticon_Gloss
-    #cough,a cough,879,COUGH
-    f = [f for f in cldf_dataset['ParameterTable'] if f['ID'] == 'cough']
+def test_parameters(cldf_dataset):
+    # ID,Name,Concepticon_ID,Concepticon_Gloss
+    # cough,a cough,879,COUGH
+    f = [f for f in cldf_dataset["ParameterTable"] if f["ID"] == "cough"]
     assert len(f) == 1
-    assert f[0]['Name'] == 'a cough'
-    assert f[0]['Concepticon_ID'] == '879'
-    assert f[0]['Concepticon_Gloss'] == 'COUGH'
+    assert f[0]["Name"] == "a cough"
+    assert f[0]["Concepticon_ID"] == "879"
+    assert f[0]["Concepticon_Gloss"] == "COUGH"
 
 
-def test_sources(cldf_dataset, cldf_logger):
-    f = [f for f in cldf_dataset.sources if f.id == 'abbott1985']
+def test_sources(cldf_dataset):
+    f = [f for f in cldf_dataset.sources if f.id == "abbott1985"]
     assert len(f) == 1
